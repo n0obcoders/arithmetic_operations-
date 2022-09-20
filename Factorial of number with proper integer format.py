a94fb1ca17c14e83c0924aa9a00ddfd1be8abb4a
n0obcoders <109712517+n0obcoders@users.noreply.github.com>
2022-09-20 16:05:36 +0530

    Create Factorial of number with proper integer format.py

---
+++ Factorial of number with proper integer format.py
@@ -0,0 +1,7 @@
+i= 1
+sum = 1
+a = int(input(" enter a number to get factorial of it even if your calculator is not able to give it to you in proper number format:\n"))
+while i <= a :
+    sum = sum * i
+    i = i +1
+print("FACTORIAL OF YOUR GIVEN NUMBER IN PROPER INTEGER FORMAT IS\n\n\n",sum)
